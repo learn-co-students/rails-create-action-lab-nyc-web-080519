3f1079d9e29d6ec2a04a87885c685321f6a15f5d
github username <github email address>
2019-08-28 23:17:38 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,10 +8,15 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def new
+  def new #creates an instance of the StudentsController and call its new method. Rails will be default render new.html.erb and will make the @student instance variable accessible to view. 
+    @student = Student.new 
   end
 
-  def create
+  def create #uses post parameters from the form and URL will be /students
+    @student = Student.new 
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save 
+    redirect_to student_path(@student)
   end
-
 end
